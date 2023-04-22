65209b3698855b29ecab6f8e9b36c0ac37e73a79
Priscila Padilla Latance -PPL <priscila.latance@iteris.com.br>
2023-04-22 10:39:05 -0300

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app-bar app color="black" dark flat class="px-12">
+    <v-btn>
+        <v-icon color="yellow" left class="mr-2">fas fa-file-signature</v-icon>Portfólio
+    </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn text @click="scroll('home')" class="text-yellow">Home</v-btn>
+    <v-btn text @click="scroll('sobre')" class="text-yellow">Sobre</v-btn>
+    <v-btn text @click="scroll('portfolio')" class="text-yellow">Portfólio</v-btn>
+    <v-btn text @click="scroll('services')" class="text-yellow">Services</v-btn>
+    <v-btn text @click="scroll('page')" class="text-yellow">Page</v-btn>
+    <v-btn text @click="scroll('blog')" class="text-yellow">Blog</v-btn>
+    <v-btn text @click="scroll('contact')" class="text-yellow">Contatos</v-btn>    
+  </v-app-bar>
+
+</template>
+
+<script>
+
+export default {
+    methods: {
+        scroll(refName){
+            const element = document.getElementById(refName);
+            element.scrollIntoView({behavior: "smooth"});
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="home">
+    <NavBar />
+    <v-container fluid>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { defineComponent } from 'vue';
 
-export default {
+// Components
+import NavBar from '@/components/NavBar.vue';
+
+export default defineComponent({
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
-}
+    NavBar
+  },
+});
 </script>
